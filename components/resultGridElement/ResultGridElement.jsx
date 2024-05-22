f25059332e719e1af5a067e68c648b1aa7e0db33
Roman Baranovskii <rom8614@gmail.com>
2024-05-22 15:03:44 +0300

    ref

--- components/resultGridElement/ResultGridElement.jsx
+++ components/resultGridElement/ResultGridElement.jsx
@@ -1,27 +1,26 @@
-import styles from './ResultGridElement.module.css';
 import {useState} from "react";
 import Image from "next/image";
-import clearIcon from '../../public/clearIcon.svg'
+import {useRouter} from "next/router";
+import clearIcon from '../../public/clearIcon.svg';
+
+import styles from './ResultGridElement.module.css';
 
 export const ResultGridElement = ({ imageUrlSmall, imageUrlFull }) => {
     const [expanded, setExpanded] = useState(false);
-
+    const { basePath } = useRouter();
     const openImage = () => {
         setExpanded(true);
     };
-
     const closeImage = () => {
         setExpanded(false);
-        console.log('closed')
     };
-
     return(
         <div className={styles.root}>
-            <Image src={imageUrlSmall} alt="gallery-image" className={styles.smallImg} width={110} height={110} onClick={openImage} />
+            <Image src={`${basePath}${imageUrlSmall}`} alt="gallery-image" className={styles.smallImg} width={110} height={110} onClick={openImage} />
             {expanded && (
                 <div className={styles.blur}>
-                    <Image className={styles.bigImg} src={imageUrlFull} alt="expanded-gallery-image" width={100} height={100} />
-                    <Image className={styles.closeImg} src={clearIcon} width={32} height={32} alt='close' onClick={closeImage} />
+                    <Image className={styles.bigImg} src={`${basePath}${imageUrlFull}`} alt="expanded-gallery-image" width={100} height={100} />
+                    <Image className={styles.closeImg} src={`${basePath}${clearIcon}`} width={32} height={32} alt='close' onClick={closeImage} />
                 </div>
             )}
         </div>
--- components/searchForm/SearchForm.jsx
+++ components/searchForm/SearchForm.jsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
-import styles from './SearchForm.module.css'
-import searchIcon from '../../public/frame12.svg'
-import clearIcon from '../../public/clearIcon.svg'
 import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
+import Image from "next/image";
+import searchIcon from '../../public/frame12.svg'
+import clearIcon from '../../public/clearIcon.svg'
+
+import styles from './SearchForm.module.css'
 
 export const SearchForm = () => {
     const router = useRouter();
-    const { query } = router.query;
+    const { basePath, query } = router;
     const [inputValue, setInputValue] = useState('');
-
     useEffect(() => {
         if (query !== null) {
             autoChange(query);
@@ -21,18 +21,15 @@ export const SearchForm = () => {
     const handleChange = (event) => {
         setInputValue(event.target.value);
     };
-
     const handleSearch = () => {
         if (inputValue.trim()) {
             router.push(`/?query=${encodeURIComponent(inputValue)}`);
         }
     }
-
     const handleClear = () => {
         setInputValue('')
         router.push(`/`);
     }
-
     const handleSubmit = (event) => {
         event.preventDefault();
         handleSearch();
@@ -42,7 +39,7 @@ export const SearchForm = () => {
         <div className={styles.root}>
             <form className={styles.form} onSubmit={handleSubmit}>
                 <div className={styles.formBlock}>
-                    <Image className={styles.searchIcon} src={searchIcon} alt={'searchIcon'} />
+                    <Image className={styles.searchIcon} src={`${basePath}${searchIcon}`} alt={'searchIcon'} />
                     <input
                         type='text'
                         placeholder="Телефоны, яблоки, груши..."
@@ -50,7 +47,7 @@ export const SearchForm = () => {
                         onChange={handleChange}
                         value={inputValue}
                     />
-                    {inputValue && <Image src={clearIcon} onClick={handleClear} className={styles.clearIcon} alt={'clearIcon'}/>}
+                    {inputValue && <Image src={`${basePath}${clearIcon}`} onClick={handleClear} className={styles.clearIcon} alt={'clearIcon'}/>}
                 </div>
                 <button type='submit' className={styles.button}>Искать</button>
             </form>
