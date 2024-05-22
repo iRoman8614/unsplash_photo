671dd8ae52c0ea64819686d52a432a90fddeebd2
Roman Baranovskii <rom8614@gmail.com>
2024-05-22 14:38:58 +0300

    build1

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,12 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ['images.unsplash.com'],
+        unoptimized: trueб
     },
+    basePath: '/unsplash_photo',
+    assetPrefix: '/unsplash_photo/',
+    trailingSlash: true,
+    output: 'export',
 };
 
 export default nextConfig;
