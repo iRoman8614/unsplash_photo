931cd643b4bf67b4f31257ca1e923520ea460a47
Roman Baranovskii <rom8614@gmail.com>
2024-05-22 14:42:54 +0300

    Updates

---
+++ _next/static/beqt0ocUkrFjJqYX3E0qZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/cc90382ef340ce65.css","static/chunks/pages/index-b0754cf5cf53f41a.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
